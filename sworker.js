6ddfb9ed2d6d084d185a8ff082129ef424a497bb
David-VGMZ <panzhojuasjuas@gmail.com>
2025-11-17 01:03:14 -0600

    Add files via upload

---
+++ sworker.js
@@ -0,0 +1,61 @@
+const CACHE_NAME = 'proyectos-pwa-v4';
+const PRECACHE_URLS = [
+  './',
+  './inicioGGresp.html?homescreen=v2',
+  './inicioGGresp.html',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+];
+
+// Instalar SW
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(PRECACHE_URLS))
+  );
+});
+
+// Activar SW (limpiar versiones viejas)
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k)))
+    )
+  );
+});
+
+// Interceptar fetch
+self.addEventListener('fetch', (event) => {
+
+  const url = event.request.url;
+
+  //EXCEPCIÓN: NO interceptar Firebase
+  if (
+    url.includes('firestore.googleapis.com') ||
+    url.includes('firebaseio.com') ||
+    url.includes('googleapis.com') ||
+    url.includes('gstatic.com')
+  ) {
+    return; // Permite que pase directo a la red
+  }
+
+  // Manejo normal de cache
+  event.respondWith(
+    caches.match(event.request, { ignoreSearch: true }).then(cached => 
+      cached ||
+      fetch(event.request).then(resp => {
+        try {
+          if (event.request.method === 'GET') {
+            const copy = resp.clone();
+            caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+          }
+        } catch (e) {}
+
+        return resp;
+      }).catch(() => {
+        if (event.request.destination === 'document') return caches.match('./index.html');
+        return new Response('', { status: 503, statusText: 'Offline' });
+      })
+    )
+  );
+});
